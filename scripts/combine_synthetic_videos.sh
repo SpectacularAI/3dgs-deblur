30a5c526f83c770c2213f02222b68c16809decd7
Otto Seiskari <otto.seiskari@gmail.com>
2024-03-19 10:42:36 +0200

    Add scripts and overlay images, tweak README

---
+++ scripts/combine_synthetic_videos.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eux
+
+BASELINE="$1"
+OURS="$2"
+OUT_FILE="$3"
+: "${OVERLAY_BASE:=overlay_images/overlay_600x400_splatfacto.png}"
+: "${OVERLAY_OURS:=overlay_images/overlay_600x400_deblurred.png}"
+
+RAW_OUT="-c:v libx264 -crf 12 -qp 0 -bsf:v h264_mp4toannexb -f mpegts -hide_banner -loglevel error -y -avoid_negative_ts make_zero"
+HI_Q_OUT="-bsf:a aac_adtstoasc -c:v libx264 -crf 21 -qp 0 -y -pix_fmt yuv420p"
+
+LEN=8
+
+ffmpeg \
+    -i "$OURS" \
+    -i "$BASELINE" \
+    -i "$OVERLAY_OURS" \
+    -i "$OVERLAY_BASE" \
+    -filter_complex \
+"[0:v][2:v]overlay[ours1];\
+ [1:v][3:v]overlay[base1];\
+ [ours1]scale=600x400,setpts=PTS-STARTPTS[base];\
+ [base1]scale=600x400,setpts=PTS-STARTPTS[overlay];\
+ color=0x00000000:s=600x400:d=$LEN,format=rgba[black];\
+ color=0xffffffff:s=600x400:d=$LEN,format=rgba[white];\
+ [black][white]blend=all_expr='if(lte(X,W*abs(1-T/$LEN*2)),B,A)':shortest=1[mask];\
+ [overlay][mask]alphamerge[overlayalpha]; \
+ [base][overlayalpha]overlay,fps=30" \
+    $HI_Q_OUT \
+    -y \
+    "$OUT_FILE"
---
+++ scripts/make_synthetic_demo_videos.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -eux
+
+SRC_FOLDER="source_videos"
+OUT_FOLDER="static/videos"
+
+mkdir -p "$OUT_FOLDER"
+cp "$SRC_FOLDER"/synthetic_*_example.mp4 "$OUT_FOLDER"
+
+for f in $SRC_FOLDER/synthetic-posenoise/baseline/*; do
+    CASE=$(basename $f)
+    OUT_FILE="$OUT_FOLDER/posenoise-$CASE.mp4"
+
+    OVERLAY_OURS=overlay_images/overlay_600x400_optimized.png ./scripts/combine_synthetic_videos.sh \
+        "$SRC_FOLDER/synthetic-posenoise/baseline/$CASE/demo_video.mp4" \
+        "$SRC_FOLDER/synthetic-posenoise-2nd-pass/baseline/$CASE/demo_video.mp4" \
+        "$OUT_FILE"
+done
+
+RENDERS_FOLDER="$SRC_FOLDER/synthetic-rs"
+
+for f in $RENDERS_FOLDER/baseline/*; do
+    CASE=$(basename $f)
+    OUT_FILE="$OUT_FOLDER/rs-$CASE.mp4"
+
+    OVERLAY_OURS=overlay_images/overlay_600x400_compensated.png ./scripts/combine_synthetic_videos.sh \
+        "$RENDERS_FOLDER/baseline/$CASE/demo_video.mp4" \
+        "$RENDERS_FOLDER/rolling_shutter/$CASE/demo_video.mp4" \
+        "$OUT_FILE"
+done
+
+RENDERS_FOLDER="$SRC_FOLDER/synthetic-mb"
+
+for f in $RENDERS_FOLDER/baseline/*; do
+    CASE=$(basename $f)
+    OUT_FILE="$OUT_FOLDER/mb-$CASE.mp4"
+
+    ./scripts/combine_synthetic_videos.sh \
+        "$RENDERS_FOLDER/baseline/$CASE/demo_video.mp4" \
+        "$RENDERS_FOLDER/motion_blur/$CASE/demo_video.mp4" \
+        "$OUT_FILE"
+done
