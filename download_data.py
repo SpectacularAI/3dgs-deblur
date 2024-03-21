29c8345a798ee6397e6527b30bc9b9e77d1994db
maturk <matiasturkulainen@gmail.com>
2024-03-21 14:41:14 +0200

    update download script with Zenodo

--- download_data.py
+++ download_data.py
@@ -1,69 +1,89 @@
-"""Script to download the datasets."""
+"""Script to download processed datasets."""
 import os
+import subprocess
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Literal
 
-import gdown
 import tyro
-from dataclasses import dataclass
+
 
 @dataclass
 class DownloadProcessedData:
     save_dir: Path = Path(os.getcwd() + "/data")
     """Save directory. Default /data."""
-    dataset: Literal[
-        "synthetic-mb",
-        "synthetic-rs",
-        "synthetic-posenoise",
-        "spectacular-rec",
-        "deblurnerf_dataset_sai",
-
-        "synthetic-all",
-        "processed-all",
-        "raw-all",
-
-    ] = "synthetic-all"
-    """Dataset download name. Set to 'synthetic-all' to download all synthetic data."""
+    dataset: Literal["synthetic", "sai"] = "synthetic"
+    """Dataset download name. Set to 'synthetic' to download all synthetic data. Set to 'spectacular' for real world smartphone captures."""
 
     def main(self):
+        self.save_dir.mkdir(parents=True, exist_ok=True)
+
         urls = {
             "inputs-processed": {
-                "synthetic-mb": "https://drive.google.com/drive/folders/1alEussfTV5f8rwKufQ0E_W86lnoFyFSG?usp=drive_link",
-                "synthetic-rs": "https://drive.google.com/drive/folders/1b6ziMo2tKFswR85Rr3l0AB18VuMAXqOe?usp=drive_link",
-                "synthetic-posenoise": "https://drive.google.com/drive/folders/1L0AwtJoLOERrkDsqSEGT8TqGRAibLNHP?usp=drive_link",
-                "colmap-sai-cli-calib-intrinsics-blur-scored": "https://drive.google.com/drive/folders/1KM72Y92DFayJsw1qBDIMAx3z-TqFrsjQ?usp=drive_link"
-            },
-            "inputs-raw": {
-                "spectacular-rec": "https://drive.google.com/drive/folders/1Fms67IPzdblI44rFZfazhLdrxfKH3sEv?usp=drive_link",
-                "deblurnerf_dataset_sai": "https://drive.google.com/drive/folders/1TPGl_Q40UDVh8_pW4cAqSnPPVeJEFxwx?usp=drive_link"
+                "synthetic-all": "https://zenodo.org/records/10847884/files/processed-nerfstudio.zip",
+                "colmap-sai-cli-orig-intrinsics-blur-scored": "https://zenodo.org/records/10848124/files/colmap-sai-cli-orig-intrinsics-blur-scored.tar.xz",
+                "colmap-sai-cli-calib-intrinsics-blur-scored": "https://zenodo.org/records/10848124/files/colmap-sai-cli-calib-intrinsics-blur-scored.tar.xz",
+                "colmap-sai-cli-vels-blur-scored.zip": "https://zenodo.org/records/10848124/files/colmap-sai-cli-vels-blur-scored.zip",
             }
         }
 
         def download_dataset(dataset):
             for subfolder, sub_urls in urls.items():
-                if dataset not in sub_urls: continue
-
-                print(f"Downloading {subfolder}/{dataset}")
-                save_dir = self.save_dir / subfolder / Path(dataset)
+                save_dir = self.save_dir / subfolder
                 save_dir.mkdir(parents=True, exist_ok=True)
-                gdown.download_folder(
-                    sub_urls[dataset], output=str(save_dir), quiet=False, remaining_ok=True
-                )
-                return
-            raise RuntimeError(f"Invalid dataset {dataset}")
+                download_command = ["wget", "-P", str(self.save_dir), sub_urls[dataset]]
 
-        if self.dataset == "synthetic-all":
+                # download
+                try:
+                    subprocess.run(download_command, check=True)
+                    print("File file downloaded succesfully.")
+                except subprocess.CalledProcessError as e:
+                    print(f"Error downloading file: {e}")
+
+                file_name = Path(sub_urls[dataset]).name
+
+                # subsubfolder for sai data
+                subsubfolder = file_name.split(".")[0] if "sai" in file_name else ""
+                if subsubfolder:
+                    Path(self.save_dir / subfolder / subsubfolder).mkdir(
+                        parents=True, exist_ok=True
+                    )
+
+                # deal with zip or tar formats
+                if Path(sub_urls[dataset]).suffix == ".zip":
+                    extract_command = [
+                        "unzip",
+                        self.save_dir / file_name,
+                        "-d",
+                        self.save_dir / Path(subfolder) / subsubfolder,
+                    ]
+                else:
+                    extract_command = [
+                        "tar",
+                        "-xvJf",
+                        self.save_dir / file_name,
+                        "-C",
+                        self.save_dir / Path(subfolder) / subsubfolder,
+                    ]
+
+                # extract
+                try:
+                    subprocess.run(extract_command, check=True)
+                    print("Extraction complete.")
+                except subprocess.CalledProcessError as e:
+                    print(f"Extraction failed: {e}")
+
+        if self.dataset == "synthetic":
             for dataset in urls["inputs-processed"].keys():
-                if 'synthetic-' in dataset:
+                if "synthetic" in dataset:
                     download_dataset(dataset)
-        elif self.dataset == "processed-all":
+        elif self.dataset == "sai":
             for dataset in urls["inputs-processed"].keys():
-                download_dataset(dataset)
-        elif self.dataset == "raw-all":
-            for dataset in urls["inputs-raw"].keys():
-                download_dataset(dataset)
+                if "sai" in dataset:
+                    download_dataset(dataset)
         else:
-            download_dataset(self.dataset)
+            raise NotImplementedError
+
 
 if __name__ == "__main__":
     tyro.cli(DownloadProcessedData).main()
